40dfefdbbadd9af43a363eb38bb61c7d701aef1a
rommel dalisay <rommelmdalisay@gmail.com>
2026-05-16 09:28:01 +0800

    item crud

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => {
-    res.send('Hello from nodejs!')
-})
+const items = require('./routes/item');
+
+// app.get('/', (req, res) => {
+//     res.send('Hello from nodejs!')
+// })
+app.use('/api/v1', items);
 
 module.exports = app
---
+++ routes/item.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const { getAllItems, } = require('../controllers/item')
+
+router.get('/items', getAllItems)
+
+module.exports = router;
