faa951c0fcb8abd811878127ab019844669c02da
rommel dalisay <rommelmdalisay@gmail.com>
2026-05-20 15:23:01 +0800

    transaction

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const items = require('./routes/item');
 const users = require('./routes/user');
+const orders = require('./routes/order');
 
 // app.get('/', (req, res) => {
 //     res.send('Hello from nodejs!')
@@ -10,5 +11,6 @@ app.use(express.json())
 
 app.use('/api/v1', items);
 app.use('/api/v1', users);
+app.use('/api/v1', orders);
 
 module.exports = app
