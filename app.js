58af20b546967f4603340ce907d69ecd0a159c7d
rommel dalisay <rommelmdalisay@gmail.com>
2026-05-20 15:29:38 +0800

    dashboard

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const items = require('./routes/item');
 const users = require('./routes/user');
 const orders = require('./routes/order');
+const dashboard = require('./routes/dashboard');
 
 // app.get('/', (req, res) => {
 //     res.send('Hello from nodejs!')
@@ -12,5 +13,6 @@ app.use(express.json())
 app.use('/api/v1', items);
 app.use('/api/v1', users);
 app.use('/api/v1', orders);
+app.use('/api/v1', dashboard);
 
 module.exports = app
