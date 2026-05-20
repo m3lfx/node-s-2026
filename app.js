f2a55446cd7884fbf2c33d9c1d8520c8348df8c4
rommel dalisay <rommelmdalisay@gmail.com>
2026-05-20 16:21:45 +0800

    item crud

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
 const items = require('./routes/item');
 const users = require('./routes/user');
 const orders = require('./routes/order');
@@ -8,7 +10,9 @@ const dashboard = require('./routes/dashboard');
 // app.get('/', (req, res) => {
 //     res.send('Hello from nodejs!')
 // })
+app.use(cors())
 app.use(express.json())
+
 
 app.use('/api/v1', items);
 app.use('/api/v1', users);
--- routes/item.js
+++ routes/item.js
@@ -2,11 +2,16 @@ const express = require('express');
 const router = express.Router();
 const { getAllItems,
     getSingleItem,
-    createItem
+    createItem,
+    updateItem,
+    deleteItem,
 } = require('../controllers/item')
 const upload = require('../utils/multer')
+const { isAuthenticatedUser } = require('../middlewares/auth')
 
 router.get('/items', getAllItems)
 router.get('/items/:id', getSingleItem)
-router.post('/items', upload.single('image'), createItem)
+router.post('/items', isAuthenticatedUser, upload.single('image'), createItem)
+router.put('/items/:id', isAuthenticatedUser, upload.single('image'), updateItem)
+router.delete('/items/:id', isAuthenticatedUser, deleteItem)
 module.exports = router;
